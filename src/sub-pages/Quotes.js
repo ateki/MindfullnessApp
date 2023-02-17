3842ad2afe16269889e477c990ed1b6512343f1b
cc-aeya <aaminah.x@icloud.com>
2023-02-17 17:59:51 +0000

    random quote generator working

--- src/sub-pages/Quotes.js
+++ src/sub-pages/Quotes.js
@@ -1,14 +1,32 @@
+import qData from '../qData.json'
+
 const styles = {
-    center: {
-        textAlign: 'center'
+    h3: {
+        textAlign: 'center',
+        marginTop: '2rem'
     }
 }
-function Quotes(){
+
+function Quotes() {
+    function generateRandomQuote() {
+      let randomQuote = qData[Math.floor(Math.random() * qData.length)];
+      return randomQuote;
+    }
+  
+    const randomQuote = generateRandomQuote();
+  
     return (
-        <>
-        <h1 style={styles.center}>Quote of the Day</h1>
-        </>
-    )
-}
+      <>
+        <div className="quoteDiv">
+        <h1 style={styles.h3}>Quotes</h1>
+          <p className="quote">"{randomQuote.quote}"</p>
+          <p className="author">- {randomQuote.author}</p>
+        </div>
+      </>
+    );
+  }
+  
+  
+  
 
 export default Quotes
