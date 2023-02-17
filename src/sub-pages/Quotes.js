d988ceb00902c1b4644cba7d4d8d3d3da1183488
cc-aeya <aaminah.x@icloud.com>
2023-02-17 11:59:19 +0000

    added quotes page and moved menu-categories into sub-pages

---
+++ src/sub-pages/Quotes.js
@@ -0,0 +1,14 @@
+const styles = {
+    center: {
+        textAlign: 'center'
+    }
+}
+function Quotes(){
+    return (
+        <>
+        <h1 style={styles.center}>Quote of the Day</h1>
+        </>
+    )
+}
+
+export default Quotes
