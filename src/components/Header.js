07f76e9b836b7f170ecadcb93c39acd5a7b19b3d
dezanidesign <116013559+dezanidesign@users.noreply.github.com>
2023-02-18 01:27:27 +0000

    Updated Garden Page and and Landing Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,28 @@
 import { NavLink } from 'react-router-dom';
+import logo from "../assets/MindfullLogo.png"
 
-function Header(){
-    return (
-        <>
-        <header>
-        <NavLink className="logo" to="/" >Logo</NavLink>
-        <nav>
-        <NavLink to="/menu">Menu</NavLink>
-        <NavLink to="/garden">Garden</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-        <NavLink to="/about">About</NavLink>
-        </nav>
 
-        </header>
-        </>
-    )
+function Header() {
+  return (
+    <div className="header">
+      <div className="logo">
+        
+        <NavLink className="logo" to="/" ><img src={logo}></img></NavLink>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li><NavLink to="/menu">MENU</NavLink></li>
+          <li className='gardenBorder'><NavLink to="/garden">GARDEN</NavLink></li>
+          <li><NavLink to="/contact">CONTACT</NavLink></li>
+          <li><NavLink to="/about">ABOUT</NavLink></li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
+
+
+
+
--- src/pages/Garden.js
+++ src/pages/Garden.js
@@ -3,17 +3,41 @@ import T3 from "../assets/T3.gif";
 import T4 from "../assets/T4.gif";
 import T5 from "../assets/T5.gif";
 // import T6 from "../assets/T6.gif";
+import T2G from "../assets/T2Grass.gif";
+import T3G from "../assets/T3Grass.gif";
+import T4G from "../assets/T4Grass.gif";
+import T5G from "../assets/T5Grass.gif";
+import T6G from "../assets/T6Grass.gif";
 import "../styles/garden.css";
 import garden from "../assets/Garden.png";
+import { useState } from "react";
 
 function Garden() {
+
+let [tree, treeGrowth] = useState(T2G);
+
+
   return (
     <div className="gardenBackground">
-      <h3>Welcome to your Garden</h3>
+      
 
-      <div className="tree">
-        <img src={T3} />
+      <button className="treeButton" onClick={() => {
+  if (tree === T2G) {
+    treeGrowth(T3G);
+  } else if (tree === T3G) {
+    treeGrowth(T4G);
+  } else if (tree === T4G) {
+    treeGrowth(T5G);
+  } else if(tree === T5G) {
+    treeGrowth(T6G)
+  } else {}
+}}>
+Let's Grow!
+</button>
+      <div >
+        <img className="tree" src={tree} />
       </div>
+      
     </div>
   );
 }
