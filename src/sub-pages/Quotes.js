a62f82d1e186a42ee5bc5fbad268c2ec25388d8e
cc-aeya <aaminah.x@icloud.com>
2023-02-18 17:04:38 +0000

    css

--- src/sub-pages/Quotes.js
+++ src/sub-pages/Quotes.js
@@ -12,13 +12,15 @@ function Quotes() {
     const randomQuote = generateRandomQuote();
   
     return (
-        <div className="quoteDiv quoteBackground">
-        <h1>Quotes</h1>
+        <div className="q-container quoteBackground">
+        <h1>Quote of the Day</h1>
+        <div className="quote-div" >
           <p className="quote">"{randomQuote.quote}"</p>
           <p className="author">- {randomQuote.author}</p>
+        </div>
 
           <div className="img-container">
-          <img className="book" src={BookIcon} />
+          <img className="book-icon" src={BookIcon} />
           </div>
         </div>
     
