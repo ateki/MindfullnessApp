32eb01e13ed903011aa8d0a573a7f52ff90dad9e
Snoopawoo <reki.csaba@gmail.com>
2023-02-18 17:57:04 +0000

    basic_music_player

--- src/sub-pages/Sounds.js
+++ src/sub-pages/Sounds.js
@@ -1,9 +1,160 @@
-function Sounds(){
-    return(
-        <>
-        <h1>Sounds</h1>
-        </>
-    )
+import React, { useState, useRef} from "react";
+import Container from "@material-ui/core/Container";
+import ReactPlayer from "react-player";
+import Controls from "../components/Controls";
+
+const format = (seconds) => {
+  if (isNaN(seconds)) {
+    return `00:00`;
+  }
+  const date = new Date(seconds * 1000);
+  const hh = date.getUTCHours();
+  const mm = date.getUTCMinutes();
+  const ss = date.getUTCSeconds().toString().padStart(2, "0");
+  if (hh) {
+    return `${hh}:${mm.toString().padStart(2, "0")}:${ss}`;
+  }
+  return `${mm}:${ss}`;
+};
+
+let count = 0;
+
+function Sounds() {
+  const [timeDisplayFormat, setTimeDisplayFormat] = React.useState("normal");
+  const [state, setState] = useState({
+    pip: false,
+    playing: true,
+    controls: false,
+    light: false,
+
+    muted: true,
+    played: 0,
+    duration: 0,
+    playbackRate: 1.0,
+    volume: 1,
+    loop: false,
+    seeking: false,
+  });
+
+  const playerRef = useRef(null);
+  const playerContainerRef = useRef(null);
+  const controlsRef = useRef(null);
+  const {
+    playing,
+    muted,
+    played,
+    volume,
+  } = state;
+
+  const handlePlayPause = () => {
+    setState({ ...state, playing: !state.playing });
+  };
+
+  const handleProgress = (changeState) => {
+    if (count > 3) {
+      controlsRef.current.style.visibility = "hidden";
+      count = 0;
+    }
+    if (controlsRef.current.style.visibility === "visible") {
+      count += 1;
+    }
+    if (!state.seeking) {
+      setState({ ...state, ...changeState });
+    }
+  };
+
+  const handleSeekChange = (e, newValue) => {
+    console.log({ newValue });
+    setState({ ...state, played: parseFloat(newValue / 100) });
+  };
+
+  const handleSeekMouseDown = (e) => {
+    setState({ ...state, seeking: true });
+  };
+
+  const handleSeekMouseUp = (e, newValue) => {
+    console.log({ value: e.target });
+    setState({ ...state, seeking: false });
+    playerRef.current.seekTo(newValue / 100, "fraction");
+  };
+
+  const handleDuration = (duration) => {
+    setState({ ...state, duration });
+  };
+
+  const handleVolumeSeekDown = (e, newValue) => {
+    setState({ ...state, seeking: false, volume: parseFloat(newValue / 100) });
+  };
+  const handleVolumeChange = (e, newValue) => {
+    setState({
+      ...state,
+      volume: parseFloat(newValue / 100),
+      muted: newValue === 0 ? true : false,
+    });
+  };
+  const handleDisplayFormat = () => {
+    setTimeDisplayFormat(
+      timeDisplayFormat === "normal" ? "remaining" : "normal"
+    );
+  };
+  const hanldeMute = () => {
+    setState({ ...state, muted: !state.muted });
+  };
+
+  const currentTime =
+    playerRef && playerRef.current
+      ? playerRef.current.getCurrentTime()
+      : "00:00";
+
+  const duration =
+    playerRef && playerRef.current ? playerRef.current.getDuration() : "00:00";
+  const elapsedTime =
+    timeDisplayFormat === "normal"
+      ? format(currentTime)
+      : `-${format(duration - currentTime)}`;
+
+  const totalDuration = format(duration);
+
+  return (
+    <>
+      <Container maxWidth="md">
+        <div
+          ref={playerContainerRef}
+         
+        >
+          <ReactPlayer
+            ref={playerRef}
+            width="0%"
+            height="0.1%"
+            url="https://www.youtube.com/watch?v=Cj2eUpt3MVE"
+            playing={playing}
+            volume={volume}
+            muted={muted}
+            onProgress={handleProgress}
+          />
+
+          <Controls
+            ref={controlsRef}
+            onSeek={handleSeekChange}
+            onSeekMouseDown={handleSeekMouseDown}
+            onSeekMouseUp={handleSeekMouseUp}
+            onDuration={handleDuration}
+            onPlayPause={handlePlayPause}
+            playing={playing}
+            played={played}
+            elapsedTime={elapsedTime}
+            totalDuration={totalDuration}
+            onMute={hanldeMute}
+            muted={muted}
+            onVolumeChange={handleVolumeChange}
+            onVolumeSeekDown={handleVolumeSeekDown}
+            onChangeDispayFormat={handleDisplayFormat}
+            volume={volume}
+          />
+        </div>
+      </Container>
+    </>
+  );
 }
 
-export default Sounds
+export default Sounds;
