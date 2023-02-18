7b59246b160483a454791e10f99591e37a5aa0fc
Irene Atek <irene.atek@btinternet.com>
2023-02-18 14:05:12 +0000

    added background image for breathing page - backgroundAttachment required to cover nav area

--- src/sub-pages/Breathing.js
+++ src/sub-pages/Breathing.js
@@ -1,8 +1,26 @@
+
+import imgBg from '../assets/Cave.png';
+
+const styles = {
+
+    backgroundImg: {
+     backgroundImage: `url(${imgBg})`,
+     backgroundPosition: 'center',
+     backgroundSize: 'cover',
+     backgroundRepeat: 'no-repeat',
+     backgroundAttachment: 'fixed',
+     width: '100vw',  // 100% of current viewpoint 
+     height: '100vh' 
+  
+   }
+ };
+ 
+
 function Breathing(){
     return(
-        <>
+        <div style={styles.backgroundImg}>
         <h1>Breathing</h1>
-        </>
+        </div>
     )
 }
 
