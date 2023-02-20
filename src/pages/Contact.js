a977c118c9032f92f4e11391e10c51ca974336e0
cc-aeya <aaminah.x@icloud.com>
2023-02-20 14:35:46 +0000

    added form el

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,24 @@
+import {useState} from 'react';
+import '../styles/contact.css'
 
 function Contact(){
+
+    const [userName, setUserName] = useState('');
+
     return (
         <>
-        <h3>Contact Us</h3>
+        <div className="contactBg contact-wrapper" >
+        <h2>Contact Us</h2>
+
+        <form onSubmit={handleSubmit} >
+            <input value={userName} type="text" placeholder="Enter Your Name" />
+            {/* add to value prop to input and set it to userName so it can be referenced above to clear the inputted search */}
+            <input value="email" type="text" placeholder="Enter Email Address" />
+            <textarea value="message" type="text" placeholder="Enter Message"></textarea>
+
+            <button>Submit</button>
+        </form>
+        </div>
         </>
     )
 }
