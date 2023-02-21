dd8d6b2d5239e22c72896bcce61671eb583e6ccd
Snoopawoo <reki.csaba@gmail.com>
2023-02-21 15:26:38 +0000

    clens up Music page

--- src/sub-pages/Music.js
+++ src/sub-pages/Music.js
@@ -4,8 +4,6 @@ import '../styles/meditations.css';
 
 
 let i;
-let j;
-let k;
 
 function Music() {
     
@@ -31,51 +29,12 @@ function Music() {
 
     };
 
-    const [style2, setStyle2] = useState("dropdown-toggle3");
-    
-
-    const changeStyle2 = () => {
-        if (j === 1) {
-            setStyle2("dropdown-toggle3");
-            j = 0;
-        }
-        else {
-            setStyle2("dropdown-toggle4");
-            j = 1;
-            }
-
-    };
-
-    const [style3, setStyle3] = useState("dropdown-toggle5");
-
-    const changeStyle3 = () => {
-        if (k === 1) {
-            setStyle3("dropdown-toggle5");
-            k = 0;
-        }
-        else {
-            setStyle3("dropdown-toggle6");
-            k = 1;
-            }
-
-    };
-    
-
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     
     const toggleDropdown = () => {
         setIsDropdownOpen(!isDropdownOpen);
     };
     
-    const [isDropdownOpen2, setIsDropdownOpen2] = useState(false);
-    const toggleDropdown2 = () => {
-        setIsDropdownOpen2(!isDropdownOpen2);
-      };
-
-      const [isDropdownOpen3, setIsDropdownOpen3] = useState(false);
-    const toggleDropdown3 = () => {
-        setIsDropdownOpen3(!isDropdownOpen3);
-      };
 
     return(
     <div className="meditationsBackground">
