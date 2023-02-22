cc79aebf395ab8c446e2177fc366c8fb2cd3950e
cc-aeya <aaminah.x@icloud.com>
2023-02-22 00:19:53 +0000

    added text

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,33 @@
+import '../styles/about.css'
 
 function About(){
     return (
-        <>
-        <h3>About</h3>
-        <p>References for Vectors from Freepik</p>
-        </>
+        <div className="aboutBg aboutWrapper">
+            <h1 className="heading">About Us</h1>
+            <div className="contentContainer" >
+
+                <h3 className="slogan"> Welcome to MindFull, this app is designed to help you focus, de-stress, and stay inspired. Our app offers a variety of features to help you cultivate mindfulness and a sense of calm in your daily life.</h3>
+
+                {/* <p className="aboutText">Our timer feature allows you to set dedicated focus time for your tasks, helping you to eliminate distractions and improve productivity.</p> */}
+                    
+                {/* <p className="aboutText">
+                The breathing exercise feature guides you through a series of deep breaths, helping you to relax and reduce stress.</p> */}
+                    
+                {/* <p className="aboutText"> 
+                Our quotes page provides daily inspiration and motivation to help you stay on track with your goals.</p> */}
+
+                {/* <p className="aboutText">
+                Our meditation page offers a variety of soothing meditations, including those for sleep and mindfulness. These guided meditations are designed to help you unwind, let go of stress, and achieve a state of inner peace.</p>  */}
+
+                {/* <p className="aboutText">
+                In addition to these features, our app includes a journaling feature, allowing you to write down your thoughts, feelings, and reflections. This helps you to gain clarity and perspective on your experiences, leading to greater self-awareness and personal growth.</p> */}
+
+                <p className="aboutText">
+                At MindFull, we are committed to helping you live a more mindful and fulfilling life. Whether you're looking to improve your productivity, reduce stress, or enhance your well-being, our app is the perfect tool to support you on your journey.</p>
+
+                <p className="aboutText">Start your journey at MindFull today</p>
+            </div>  
+        </div>
     )
 }
 
